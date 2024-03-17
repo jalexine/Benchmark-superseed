3a20aa32cefc1b49324f9156378e911a56cf3c07
jalexine <127459834+jalexine@users.noreply.github.com>
2024-03-17 21:03:45 +0100

    kmc + kmctools

--- KMC3.2/bin/superkmc.sh
+++ KMC3.2/bin/superkmc.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 output1 output2"
+    echo "U\033[95m♡Pls use: $0 output1 output2 \033[0m"
     exit 1
 fi
 
 output1="$1"
 output2="$2"
 
+if [ ! -f "$output1" ] || [ ! -f "$output2" ]; then
+    echo "files pb"
+    exit 1
+fi
+
 path_kmc="/Users/alexine/chaosmer/KMC3.2/bin/kmc"
-output_folder="/Users/alexine/chaosmer/"
-
-$path_kmc -k21 "$output1" "$output1" "${output_folder}"
-
-$path_kmc -k21 "$output2" "$output2" "${output_folder}"
+$path_kmc -k21 -ci1 -cs1000 "$output1" "$output1.kmc"
+$path_kmc -k21 -ci1 -cs1000 "$output2" "$output2.kmc"
 
 #path_kmc_tools="/Users/alexine/chaosmer/KMC3.2/bin/kmc_tools"
-#$path_kmc_tools intersect "$output1"_kmc -ci10 -cx200 "$output2"_kmc -ci4 -cx100 "$output1"_"$output2"_intersect -ci20 -cx150
+#$path_kmc_tools intersect "$output1.kmc" "$output2.kmc" "$output1"_"$output2"_intersect
 
