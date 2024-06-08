d95bc7288b4f16c6d6befc0d49e1700e276241e9
jalexine <127459834+jalexine@users.noreply.github.com>
2024-06-08 18:28:58 +0200

    updated

--- plot_repeatedkmer.py
+++ plot_repeatedkmer.py
@@ -2,39 +2,32 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_repeated_kmers(file1, file2, output_dir):
-    # Read the files
-    df1 = pd.read_csv(file1, header=None, names=['file', 'count'])
-    df2 = pd.read_csv(file2, header=None, names=['file', 'count'])
+def plot_repeated_kmers(file, output_file):
+    df = pd.read_csv(file, header=None, names=['file', 'count'])
 
-    # Plot the data
+    # Extract the meaningful part of the filename
+    title = file.split('/')[-1].replace('repeatedkmer_', '').replace('.txt', '')
+
     plt.figure(figsize=(10, 6))
 
-    # Plot the data points with markers, but start plotting from the second point
-    plt.plot(df1.index + 1, df1['count'], label=f'{file1}', color='#FF69B4')
-    plt.plot(df1.index[1:] + 1, df1['count'][1:], marker='o', label='_nolegend_', color='#FF69B4')
-    plt.plot(df2.index + 1, df2['count'], label=f'{file2}', color='purple')
-    plt.plot(df2.index[1:] + 1, df2['count'][1:], marker='o', label='_nolegend_', color='purple')
+    plt.plot(df.index + 1, df['count'], color='#FF69B4')
+    plt.plot(df.index[1:] + 1, df['count'][1:], marker='o', color='#FF69B4')
 
     plt.xlabel('N Values')
-    plt.ylabel('Repeated k-mers for Different N')
-    plt.title('Repeated k-mers vs N')
-    plt.legend()
+    plt.ylabel('Repeated k-mers')
+    plt.title(f'Repeated k-mers vs N for {title}')
     plt.grid(True)
 
-    # Set x-axis limits to start from 1
-    plt.xlim(1, max(len(df1), len(df2)))
+    plt.xlim(1, len(df))
 
-    # Save the plots
-    output_file1 = f"{output_dir}/{file1.split('/')[-1].replace('.txt', '_plot.png')}"
-    output_file2 = f"{output_dir}/{file2.split('/')[-1].replace('.txt', '_plot.png')}"
-
-    plt.savefig(output_file1)
-    plt.savefig(output_file2)
-    plt.show()
+    plt.savefig(output_file)
+    plt.close()
 
 if __name__ == "__main__":
     file1 = sys.argv[1]
     file2 = sys.argv[2]
-    output_dir = sys.argv[3]
-    plot_repeated_kmers(file1, file2, output_dir)
+    output_file1 = sys.argv[3]
+    output_file2 = sys.argv[4]
+
+    plot_repeated_kmers(file1, output_file1)
+    plot_repeated_kmers(file2, output_file2)
